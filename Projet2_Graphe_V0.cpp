2bf0d5b3f5c00ed98d13f656c2572714a627c59f
Virgile-VV <75625115+Virgile-VV@users.noreply.github.com>
2021-05-15 01:41:25 +0200

    Update Projet2_Graphe_V0.cpp

--- Projet2_Graphe_V0.cpp
+++ Projet2_Graphe_V0.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include "Cgraphe.h"
 #include "Carc.h"
 #include "Csommet.h"
 using namespace std;
@@ -9,36 +10,86 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-	Carc ArcVersS1(1);
-	Carc Arc2VersS1(1);
-	Carc Arc3VersS3(3);
+
+	std::cout << "Hello Virgile!\n";
+
+	/*
+	Carc ArcS2VersS1(1);
+	Carc IIeArcS2VersS1(1);
+	Carc ArcS2VersS3(3);
 
 	Csommet Sommet1(1);
 	Csommet Sommet2(2);
 	Csommet Sommet3(3);
 
-	Sommet2.SOMAjouteArcPartant(Arc3VersS3);
-	Sommet3.SOMAjouteArcArrivant(Arc3VersS3);
+	if (Sommet2.SOMAjouteArcPartant(ArcS2VersS3)) {
+		Sommet3.SOMAjouteArcArrivant(ArcS2VersS3);
+	}
 
-	Sommet2.SOMAjouteArcPartant(ArcVersS1);
-	Sommet2.SOMAjouteArcPartant(Arc2VersS1);
 
+	if (Sommet2.SOMAjouteArcPartant(ArcS2VersS1)) {
+		Sommet1.SOMAjouteArcArrivant(ArcS2VersS1);
+	}
+
+	if (Sommet2.SOMAjouteArcPartant(IIeArcS2VersS1)) {
+		Sommet1.SOMAjouteArcArrivant(IIeArcS2VersS1);
+	}
+
+
+	cout << "Le sommet " << Sommet1.SOMLireSommet() << " pointe les sommets : ";
+	Sommet1.SOMAfficherPartant();
+	cout << "Le sommet " << Sommet1.SOMLireSommet() << " est pointe par  : ";
+	Sommet1.SOMAfficherNbArrivant();
+	cout << " sommet(s)" << endl;
 	cout << endl;
-	cout << "Le sommet " << Sommet2.SOMLireSommet() << " pointe les sommets : " ;
+
+	cout << "Le sommet " << Sommet2.SOMLireSommet() << " pointe les sommets : ";
 	Sommet2.SOMAfficherPartant();
+	cout << "Le sommet " << Sommet2.SOMLireSommet() << " est pointe par  : ";
+	Sommet2.SOMAfficherNbArrivant();
+	cout << " sommet(s)" << endl;
+	cout << endl;
+
+
+	cout << "Le sommet " << Sommet3.SOMLireSommet() << " pointe les sommets : ";
+	Sommet3.SOMAfficherPartant();
 	cout << "Le sommet " << Sommet3.SOMLireSommet() << " est pointe par  : ";
 	Sommet3.SOMAfficherNbArrivant();
-	cout << " sommet(s)"<<endl; 
+	cout << " sommet(s)" << endl;
+	cout << endl;
+
+	*/
+
+	Cgraphe graphe1;
+	Csommet SOMdepart(10);
+	Csommet SOMarrive(20);
+	Csommet SOMarrive2(30);
+
+	graphe1.GRAAjouterSommet(SOMdepart);
+	graphe1.GRAAjouterSommet(SOMarrive);
+	graphe1.GRAAjouterSommet(SOMarrive2);
+
+	graphe1.GRAAjouterArc(SOMdepart, SOMarrive);
+	graphe1.GRAAjouterArc(SOMdepart, SOMarrive2);
+
+	cout << endl;
+
+	cout << "afficher graphe :" << endl;
+	graphe1.GRAAfficherGraphe();
+	/*
+	cout << "Le sommet " << SOMdep.SOMLireSommet() << " pointe les sommets : ";
+	(graphe1.GRALireTableauSommet(0))->SOMAfficherPartant();
+	cout << "Le sommet " << SOMdep.SOMLireSommet() << " est pointe par  : ";
+	SOMdep.SOMAfficherNbArrivant();
+	cout << " sommet(s)" << endl;
+	cout << endl;
+
+	cout << "Le sommet " << SOMarr.SOMLireSommet() << " pointe les sommets : ";
+	SOMarr.SOMAfficherPartant();
+	cout << "Le sommet " << SOMarr.SOMLireSommet() << " est pointe par  : ";
+	SOMarr.SOMAfficherNbArrivant();
+	cout << " sommet(s)" << endl;
+	cout << endl;
+	*/
+
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
